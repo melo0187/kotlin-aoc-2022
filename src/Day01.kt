6290cc71ec51d21d36b7b3cfca35271e05fcc708
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2022-12-11 22:55:59 +0100

    Solved Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun <T> List<String>.chunkedByBlank(transform: ((String) -> T)) =
+        fold(mutableListOf(mutableListOf<T>())) { acc, line ->
+            when {
+                line.isNotBlank() -> acc.last().add(transform(line))
+                else -> acc.add(mutableListOf())
+            }
+            acc
+        }.map {
+            it.toList()
+        }.toList()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.chunkedByBlank(String::toInt)
+            .maxOf { elvesCalories ->
+                elvesCalories.sum()
+            }
+
+    fun part2(input: List<String>): Int =
+        input.chunkedByBlank(String::toInt)
+            .map { elvesCalories -> elvesCalories.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
