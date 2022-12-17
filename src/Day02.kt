5f320cfca1cc2e00a01c6235fc53cac33c929d8a
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2022-12-17 23:08:28 +0100

    Solved Day02 - part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,25 +1,10 @@
-import Shape.Companion.elvenShapeCodes
-import Shape.Companion.shapeByCode
-import java.lang.IllegalArgumentException
+import Codes.elvenShapeCodes
+import Codes.outcomeByCode
+import Codes.shapeByCode
 
 sealed interface Shape {
     val score: Int
     val defeat: Shape
-
-    companion object {
-        val elvenShapeCodes = listOf('A', 'B', 'C')
-        private val myShapeCodes = listOf('X', 'Y', 'Z')
-
-        fun shapeByCode(code: Char): Shape {
-            val shapeCodes = elvenShapeCodes.zip(myShapeCodes)
-            return when (code) {
-                in shapeCodes[0].toList() -> ROCK
-                in shapeCodes[1].toList() -> PAPER
-                in shapeCodes[2].toList() -> SCISSORS
-                else -> throw IllegalArgumentException("Code is not a valid shape code!")
-            }
-        }
-    }
 
     object ROCK : Shape {
         override val score: Int = 1
@@ -36,13 +21,25 @@ sealed interface Shape {
         override val defeat: Shape = PAPER
     }
 
-    fun match(opponentShape: Shape): Outcome {
-        return when (opponentShape) {
+    fun matchWith(opponentShape: Shape): Outcome =
+        when (opponentShape) {
             this -> Outcome.DRAW
             defeat -> Outcome.WIN
             else -> Outcome.LOSS
         }
+
+    fun findMatchingShapeFor(outcome: Outcome): Shape = when (outcome) {
+        Outcome.LOSS -> defeat
+        Outcome.DRAW -> this
+        Outcome.WIN -> findShapeThatDefeats(this)
     }
+
+    private tailrec fun findShapeThatDefeats(shape: Shape): Shape =
+        if (shape.defeat == this) {
+            shape
+        } else {
+            findShapeThatDefeats(shape.defeat)
+        }
 
     enum class Outcome(val score: Int) {
         LOSS(0),
@@ -51,29 +48,59 @@ sealed interface Shape {
     }
 }
 
+object Codes {
+    val elvenShapeCodes = listOf('A', 'B', 'C')
+    private val myShapeCodes = listOf('X', 'Y', 'Z')
+
+    val shapeByCode = (elvenShapeCodes + myShapeCodes)
+        .associate {
+            when (it) {
+                'A', 'X' -> it to Shape.ROCK
+                'B', 'Y' -> it to Shape.PAPER
+                'C', 'Z' -> it to Shape.SCISSORS
+                else -> error("Unknown shape code!")
+            }
+        }
+
+    val outcomeByCode =
+        mapOf(
+            'X' to Shape.Outcome.LOSS,
+            'Y' to Shape.Outcome.DRAW,
+            'Z' to Shape.Outcome.WIN
+        )
+}
+
 fun main() {
-    fun List<String>.toShapeMatchups() = map { line ->
+    fun List<String>.toCodePairs() = map { line ->
         val (elfCode, myCode) = line.slice(listOf(0, 2))
             .partition { shapeCode -> shapeCode in elvenShapeCodes }
 
         elfCode.single() to myCode.single()
-    }.map { (elfCode, myCode) ->
-        shapeByCode(elfCode) to shapeByCode(myCode)
     }
 
     fun part1(input: List<String>): Int =
-        input.toShapeMatchups()
+        input.toCodePairs()
+            .map { (elfCode, myCode) ->
+                shapeByCode.getValue(elfCode) to shapeByCode.getValue(myCode)
+            }
             .sumOf { (elfShape, myShape) ->
-                myShape.score + myShape.match(elfShape).score
+                myShape.score + myShape.matchWith(elfShape).score
             }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.toCodePairs()
+            .map { (elfCode, myCode) ->
+                shapeByCode.getValue(elfCode) to outcomeByCode.getValue(myCode)
+            }
+            .sumOf { (elfShape, myDesiredOutcome) ->
+                val myDesiredShape = elfShape.findMatchingShapeFor(myDesiredOutcome)
+                myDesiredShape.score + myDesiredShape.matchWith(elfShape).score
+            }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day02")
     part1(input).println()
