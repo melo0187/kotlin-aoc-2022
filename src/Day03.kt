6188f6fb92293eb19f48ed5ab491aae569c3ea9f
itd: Carmelo Scollo <carmelo.scollo@meisterplan.com>
2022-12-20 22:10:42 +0100

    Solved Day03 - part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -18,13 +18,19 @@ fun main() {
             priorityByItemType.getValue(commonItemType)
         }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = input
+        .map(String::toCharArray)
+        .chunked(3) { (rucksack1, rucksack2, rucksack3) ->
+            (rucksack1 intersect rucksack2.toSet() intersect rucksack3.toSet()).single()
+        }
+        .sumOf { itemTypeCorrespondingToGroupBadge ->
+            priorityByItemType.getValue(itemTypeCorrespondingToGroupBadge)
+        }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     part1(input).println()
