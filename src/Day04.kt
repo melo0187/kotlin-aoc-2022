452a0bc7aed75ae1a6fab101df5f4eabe8a9f5d9
Carmelo Scollo <carmelo.scollo@gmail.com>
2022-12-26 20:58:00 +0100

    Solved Day04 - part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,36 +1,42 @@
 fun main() {
-    fun String.toSectionIdList(): List<Int> =
-        let {
-            split('-', limit = 2)
-                .takeIf { it.size == 2 }
-                ?: throw IllegalArgumentException("String does not describe a section assignment")
-        }
-            .map(String::toInt)
-            .let { (rangeStart, rangeEnd) ->
-                (rangeStart..rangeEnd).toList()
-            }
 
-    fun part1(input: List<String>): Int = input
-        .map { sectionAssignmentPair ->
+    fun List<String>.toSectionAssignmentPairs(): List<Pair<List<Int>, List<Int>>> {
+        fun String.toSectionIdList(): List<Int> =
+            let {
+                split('-', limit = 2)
+                    .takeIf { it.size == 2 }
+                    ?: throw IllegalArgumentException("String does not describe a section assignment")
+            }
+                .map(String::toInt)
+                .let { (rangeStart, rangeEnd) ->
+                    (rangeStart..rangeEnd).toList()
+                }
+
+        return map { sectionAssignmentPair ->
             sectionAssignmentPair.split(',', limit = 2)
                 .takeIf { it.size == 2 }
                 ?: throw IllegalArgumentException("String does not describe a section assignment pair")
-        }
-        .map { (firstElfSectionAssignment, secondElfSectionAssignment) ->
+        }.map { (firstElfSectionAssignment, secondElfSectionAssignment) ->
             firstElfSectionAssignment.toSectionIdList() to secondElfSectionAssignment.toSectionIdList()
         }
+    }
+
+    fun part1(input: List<String>): Int = input.toSectionAssignmentPairs()
         .count { (firstElfSectionIdList, secondElfSectionIdList) ->
             firstElfSectionIdList.containsAll(secondElfSectionIdList) ||
                     secondElfSectionIdList.containsAll(firstElfSectionIdList)
         }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = input.toSectionAssignmentPairs()
+        .count { (firstElfSectionIdList, secondElfSectionIdList) ->
+            (firstElfSectionIdList intersect secondElfSectionIdList.toSet()).isNotEmpty()
+        }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     part1(input).println()
