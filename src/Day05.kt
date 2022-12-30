210d8223a82f6322203ccc039e42cd2879c73674
Carmelo Scollo <carmelo.scollo@gmail.com>
2022-12-30 12:21:30 +0100

    Solved Day05 - part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,66 +1,106 @@
 import java.util.Stack
 
-fun main() {
-    fun part1(input: List<String>): String {
-        val startingStacksDrawing = input.takeWhile(String::isNotBlank)
+object CrateMover9000 {
+    operator fun invoke(crateCount: Int, srcStack: Stack<Char>, targetStack: Stack<Char>) {
+        repeat(crateCount) {
+            srcStack.pop()
+                .let(targetStack::push)
+        }
+    }
+}
 
-        val stackCount = startingStacksDrawing
-            .takeLast(1)
-            .single()
-            .toList()
-            .mapNotNull(Char::digitToIntOrNull)
-            .max()
-
-        val startingStacksSlices = startingStacksDrawing
-            .dropLast(1)
-            .map { drawingLine ->
-                drawingLine
-                    .windowed(3, 4)
-                    .map { cargo ->
-                        cargo
-                            .removeSurrounding("[", "]")
-                            .singleOrNull()
-                    }
+object CrateMover9001 {
+    operator fun invoke(crateCount: Int, srcStack: Stack<Char>, targetStack: Stack<Char>) {
+        (1..crateCount)
+            .fold(mutableListOf<Char>()) { buffer, _ ->
+                srcStack.pop().let(buffer::add)
+                buffer
             }
+            .reversed()
+            .let(targetStack::addAll)
+    }
+}
+
+fun main() {
+    fun List<String>.toStacks(): Array<Stack<Char>> {
+        val stackCount =
+            takeLast(1)
+                .single()
+                .toList()
+                .mapNotNull(Char::digitToIntOrNull)
+                .max()
+
+        val startingStacksSlices =
+            dropLast(1)
+                .map { drawingLine ->
+                    drawingLine
+                        .windowed(3, 4)
+                        .map { crate ->
+                            crate
+                                .removeSurrounding("[", "]")
+                                .singleOrNull()
+                        }
+                }
 
         val startingStacks = startingStacksSlices
-            .foldRight(Array<Stack<Char>>(stackCount) { Stack() }) { sliceAcrossStacks, stacks  ->
+            .foldRight(Array<Stack<Char>>(stackCount) { Stack() }) { sliceAcrossStacks, stacks ->
                 sliceAcrossStacks.forEachIndexed { stackIndex, crate ->
                     crate?.let(stacks[stackIndex]::push)
                 }
                 stacks
             }
+        return startingStacks
+    }
+
+    fun Array<Stack<Char>>.rearrangeStacks(
+        rearrangementProcedure: List<String>,
+        pickupAndMove: (crateCount: Int, srcStack: Stack<Char>, targetStack: Stack<Char>) -> Unit
+    ) {
+        rearrangementProcedure
+            .forEach { step ->
+                val (crateCount, srcStackNumber, targetStackNumber) =
+                    step
+                        .split(' ')
+                        .mapNotNull(String::toIntOrNull)
+                val srcStack = get(srcStackNumber - 1)
+                val targetStack = get(targetStackNumber - 1)
+
+                pickupAndMove(crateCount, srcStack, targetStack)
+            }
+    }
+
+    fun part1(input: List<String>): String {
+        val startingStacksDrawing = input.takeWhile(String::isNotBlank)
+        val rearrangementProcedure = input.takeLastWhile(String::isNotBlank)
+
+        val startingStacks = startingStacksDrawing.toStacks()
 
         return with(startingStacks) {
-            input.takeLastWhile(String::isNotBlank)
-                .let { rearrangementProcedures ->
-                    rearrangementProcedures.forEach { procedure ->
-                        val (crateCount, srcStackNumber, targetStackNumber) =
-                            procedure
-                                .split(' ')
-                                .mapNotNull(String::toIntOrNull)
-                        val srcStack = get(srcStackNumber - 1)
-                        val targetStack = get(targetStackNumber - 1)
-
-                        repeat(crateCount) {
-                            srcStack.pop()
-                                .let(targetStack::push)
-                        }
-                    }
-                }
+            rearrangeStacks(rearrangementProcedure, CrateMover9000::invoke)
 
             mapNotNull(Stack<Char>::lastOrNull)
                 .joinToString("")
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): String {
+        val startingStacksDrawing = input.takeWhile(String::isNotBlank)
+        val rearrangementProcedure = input.takeLastWhile(String::isNotBlank)
+
+        val startingStacks = startingStacksDrawing.toStacks()
+
+        return with(startingStacks) {
+            rearrangeStacks(rearrangementProcedure, CrateMover9001::invoke)
+
+            mapNotNull(Stack<Char>::lastOrNull)
+                .joinToString("")
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == "CMZ")
+    check(part2(testInput) == "MCD")
 
     val input = readInput("Day05")
     part1(input).println()
